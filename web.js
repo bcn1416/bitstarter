ce2a6c180705268c5d67bef37d106bac785a7ec7
Julian <bcn1416@gmail.com>
2013-07-10 19:16:14 +0000

    Update web.js

--- web.js
+++ web.js
@@ -1,12 +1,18 @@
 var express = require('express');
+var fs = require('fs');
+var path = require('path');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+	var url = '/index.html';
+	path.join(__dirname, url);
+	response.send((fs.readFileSync(url)).toString());
+	//response.send((fs.readFileSync('index.html')).toString());
+	//response.send('Hello World!');
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+	console.log("Listening on port: " + port);
 });
